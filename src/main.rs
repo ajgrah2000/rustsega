e3c3cfda9bdd3af8f10e532bc5c624506acc6666
ajgrah2000 <ajgrah2000@localhost>
2022-09-13 00:44:50 +1000

    Adding simple read cartridge function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
+mod sega;
+
+fn usage() {
+    println!("Usage: <prog> <rom>");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        usage();
+    } else{
+        let filename = &args[1];
+
+        match sega::memory::cartridge::load(filename)
+        {
+            Ok(()) => {println!("Ok");}
+            _ => {println!("Error loading cartridge.");}
+        }
+        println!("Finished.");
+    }
 }
---
+++ src/sega/memory/cartridge.rs
@@ -0,0 +1,87 @@
+use std::fs::{File};
+use std::io::Read;
+use std::io::BufReader;
+use std::io::ErrorKind;
+
+type BankSizeType = u16;
+type NumBanksType = u8;
+
+static NUM_RAM_AGES:           u8 = 2;
+const  BANK_SIZE:    BankSizeType = 0x4000;
+const  MAX_BANKS:    NumBanksType = 64;
+static LOWERMASK:             u16 = 0x03FFF;
+
+#[derive(Copy, Clone)]
+struct Bank
+{
+    data: [u8; BANK_SIZE as usize]
+}
+
+struct Banks
+{
+    data: [u8; BANK_SIZE as usize]
+}
+
+struct Cartridge
+{
+    filename: String,
+    ram: Vec<u8>,
+    max_banks: u8,
+    num_banks: NumBanksType,
+    num_ram_pages: u8,
+    rom: [Bank; MAX_BANKS as usize]
+}
+
+fn print(cartridge: &Cartridge) {
+    for i in 0..cartridge.num_banks as usize {
+        for b in cartridge.rom[i].data {
+//            print!("{}", b);
+        }
+    }
+
+    println!("");
+    println!("read: {}", cartridge.filename);
+    println!("Num banks: {}", cartridge.num_banks);
+}
+
+pub fn load(filename: &str) -> std::io::Result<()> {
+    let mut file = File::open(filename)?;
+    let mut cartridge = Cartridge {filename:filename.to_string(), ram:Vec::new(), max_banks:0, num_banks:0, num_ram_pages:0, rom:[Bank{data:[0; BANK_SIZE as usize]}; MAX_BANKS as usize]};
+
+    (cartridge.rom, cartridge.num_banks) = read_banks(&mut file);
+
+    print(&cartridge);
+
+    Ok(())
+}
+
+fn read_banks(source: &mut dyn Read) -> ([Bank; MAX_BANKS as usize], NumBanksType) {
+    let mut banks = [Bank{data:[0; BANK_SIZE as usize]}; MAX_BANKS as usize];
+    let mut num_banks = 0;
+
+    for i in 0..MAX_BANKS {
+        match read_bank(source)
+        {
+            (Some(bank), n) => {banks[i as usize] = bank;
+                                num_banks = num_banks + 1},
+            _ => {}
+        }
+    }
+
+    (banks, num_banks)
+}
+
+fn read_bank(source: &mut dyn Read) -> (Option<Bank>, NumBanksType) {
+    let mut bank = Bank {data: [0; BANK_SIZE as usize]};
+
+    // Try to read an entire bank.
+    match source.read(&mut bank.data)
+    {
+        Ok(0) => {(None, 0)},
+        Ok(n) if n < BANK_SIZE as usize => {
+            println!("Bank incomplete ({} bytes found in last bank), will be padded with zeros", n);
+           (Some(bank), n as NumBanksType)},
+        Ok(n) => {(Some(bank), n as NumBanksType)},
+        _ => {(None, 0)}
+    }
+}
