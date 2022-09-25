b828077f40f99a0e06c6588de9c5440dd14c0ac3
ajgrah2000 <ajgrah2000@localhost>
2022-09-25 22:09:53 +1000

    Added an argument parser to simplify switching debug output on/off.

--- src/main.rs
+++ src/main.rs
@@ -3,46 +3,47 @@
 #![allow(unused_variables)]
 use std::env;
 
-mod sega;
+use argparse;
 
-fn usage() {
-    println!("Usage: <prog> <rom>");
-}
+mod sega;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut debug = false;
+    let mut cartridge_name = String::new();
 
-    if args.len() <= 1 {
-        usage();
-    } else{
-        let filename = &args[1];
-
-        let mut cartridge = sega::memory::cartridge::Cartridge::new(filename);
-        match cartridge.load()
-        {
-            Ok(()) => {println!("Ok");}
-            _ => {println!("Error loading cartridge.");}
-        }
-
-        let clock = sega::clocks::Clock::new();
-        let mut memory = sega::memory::memory::MemoryAbsolute::new();
-        let pc_state = sega::cpu::pc_state::PcState::new();
-        let vdp = sega::graphics::vdp::VDP::new();
-        let mut ports = sega::ports::Ports::new();
-        let interruptor = sega::interruptor::Interruptor::new();
-
-        // Add the graphics device to the list of ports.
-        ports.add_device(Box::new(vdp));
-
-        memory.set_cartridge(cartridge);
-        let mut core = sega::cpu::core::Core::new(clock, memory, pc_state, ports, interruptor);
-
-        let debug = true;
-
-        for _i in 0..1500000 {
-            core.step(debug);
-        }
-
-        println!("Finished.");
+    {
+        // Handle command line arguments.
+        let mut ap = argparse::ArgumentParser::new();
+        ap.set_description("Rusty Sega Emulator");
+        ap.refer(&mut debug).add_option(&["-d","--debug"], argparse::StoreTrue, "Print PC State Debug Info");
+        ap.refer(&mut cartridge_name).add_argument("cartridge", argparse::Store, "Name of cratridge to run").required();
+        ap.parse_args_or_exit();
     }
+
+    let mut cartridge = sega::memory::cartridge::Cartridge::new(&cartridge_name);
+    match cartridge.load()
+    {
+        Ok(()) => {println!("Ok");}
+        _ => {println!("Error loading cartridge.");}
+    }
+
+    let clock = sega::clocks::Clock::new();
+    let mut memory = sega::memory::memory::MemoryAbsolute::new();
+    let pc_state = sega::cpu::pc_state::PcState::new();
+    let vdp = sega::graphics::vdp::VDP::new();
+    let mut ports = sega::ports::Ports::new();
+    let interruptor = sega::interruptor::Interruptor::new();
+
+    // Add the graphics device to the list of ports.
+    ports.add_device(Box::new(vdp));
+
+    memory.set_cartridge(cartridge);
+    let mut core = sega::cpu::core::Core::new(clock, memory, pc_state, ports, interruptor);
+
+    for _i in 0..1500000 {
+        core.step(debug);
+    }
+
+    println!("Finished.");
 }
