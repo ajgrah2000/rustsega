8f4561a52f29ab9311b4f6339e19c50052137caa
ajgrah2000 <ajgrah2000@localhost>
2022-10-08 14:58:46 +1100

    Removed some commented out code.

--- src/sega/interruptor.rs
+++ src/sega/interruptor.rs
@@ -16,27 +16,7 @@ pub trait PollForInterrupt {
     fn poll_interrupts(&mut self, clock: &clocks::Clock) -> bool;
 }
 
-impl Interruptor {
-    //    pub fn interrupt<M>(pc_state: &mut pc_state::PcState, memory: &mut M)
-    //    where
-    //        M: memory::MemoryRW,
-    //    {
-    //        if pc_state.get_iff1() {
-    //            if pc_state.get_im() == 1 {
-    //                pc_state.increment_sp(-1);
-    //                memory.write(pc_state.sp_reg.get(), pc_state.get_pc_high());
-    //                pc_state.increment_sp(-1);
-    //                memory.write(pc_state.sp_reg.get(), pc_state.get_pc_low());
-    //                pc_state.set_pc(core::Core::<M>::IRQIM1ADDR as u16);
-    //
-    //                // Disable mask-able interrupts
-    //                pc_state.set_iff1(false);
-    //            } else {
-    //                panic!("Unsupported interrupt mode: {}", pc_state.get_im());
-    //            }
-    //        }
-    //    }
-}
+impl Interruptor {}
 
 impl Interruptor {
     pub fn new() -> Self {
