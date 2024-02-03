85985ab893d756b1302b07d7a46fcf228fad2351
me <a@localhost>
2024-02-03 16:05:20 +1100

    Switched sound to be 'stereo'

--- src/sega/audio/sound.rs
+++ src/sega/audio/sound.rs
@@ -7,11 +7,11 @@ pub struct SDLUtility {}
 impl SDLUtility {
     // TODO: Fix up values, make them more dynamic, do better comparisons
     // Not sure how they compare on different PCs
-    const TARGET_QUEUE_LENGTH: u32 = 2048; // This drives the 'delay' in audio, but too small for the speed and they aren't filled fast enough
+    const TARGET_QUEUE_LENGTH: u32 = 8192; // This drives the 'delay' in audio, but too small for the speed and they aren't filled fast enough
     const AUDIO_SAMPLE_SIZE: u16 = 1024; // 'Desired' sample size, too small and SDL buffer doesn't stay filled (pops/crackles).
     const FRACTION_FILL: f32 = 0.05; // TODO: FUDGE FACTOR.  Don't completely fill, samples a removed 1 at a time, don't fill them immediately.
 
-    const MONO_STERO_FLAG: u8 = 1; // TODO: Make this configurable 1 - mono, 2 - stereo
+    const MONO_STERO_FLAG: u8 = 2; // TODO: Make this configurable 1 - mono, 2 - stereo
 
     pub fn get_audio_queue(sdl_context: &mut sdl2::Sdl) -> Option<Box<SoundQueueType>> {
         let audio_subsystem = sdl_context.audio().unwrap();
